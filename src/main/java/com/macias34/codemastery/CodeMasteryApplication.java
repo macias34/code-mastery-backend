4923a2783a310d2dcff710851aa0a3e54c38ddd3
macias34 <propiesek@gmail.com>
2023-09-05 20:03:14 +0200

    upload

---
+++ src/main/java/com/macias34/codemastery/CodeMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.macias34.codemastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeMasteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macias34/codemastery/CodeMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macias34.codemastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
