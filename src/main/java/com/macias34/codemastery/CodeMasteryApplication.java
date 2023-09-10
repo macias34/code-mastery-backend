c32fcbb0691fa8b032bcbd241c014b1873f69cdf
macias34 <propiesek@gmail.com>
2023-09-10 13:43:11 +0200

    Added dotenv package to handle adding env variables to application.properties

--- src/main/java/com/macias34/codemastery/CodeMasteryApplication.java
+++ src/main/java/com/macias34/codemastery/CodeMasteryApplication.java
@@ -1,13 +1,20 @@
 package com.macias34.codemastery;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvBuilder;
 
 @SpringBootApplication
 public class CodeMasteryApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = new DotenvBuilder().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(CodeMasteryApplication.class, args);
 	}
 
