311258b63709aabfedccd15d20546cac70132b2e
macias34 <propiesek@gmail.com>
2023-09-12 15:58:46 +0200

    added find by username method to User reposit

--- src/main/java/com/macias34/codemastery/user/repository/UserRepository.java
+++ src/main/java/com/macias34/codemastery/user/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.macias34.codemastery.user.repository;
 
 import com.macias34.codemastery.user.entity.UserEntity;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+	Optional<UserEntity> findByUsername(String username);
 
+	Boolean existsByUsername(String username);
 }
