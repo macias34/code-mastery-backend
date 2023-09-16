61d2c2531743201db399d74330b08eed677912c4
macias34 <propiesek@gmail.com>
2023-09-16 21:33:36 +0200

    Added CorsConfig

--- src/main/java/com/macias34/codemastery/security/config/SecurityConfig.java
+++ src/main/java/com/macias34/codemastery/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.macias34.codemastery.security.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.macias34.codemastery.security.jwt.JwtAuthenticationFilter;
 import com.macias34.codemastery.security.jwt.JwtEntryPoint;
@@ -71,4 +76,5 @@ public class SecurityConfig {
 	public JwtAuthenticationFilter jwtAuthenticationFilter() {
 		return new JwtAuthenticationFilter();
 	}
+
 }
