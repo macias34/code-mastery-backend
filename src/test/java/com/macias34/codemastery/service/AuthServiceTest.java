36633d5674a1077bb310b1917fc1899cdbd7ca90
macias34 <propiesek@gmail.com>
2023-09-16 12:31:43 +0200

    Added shouldThrowExceptionWhenUserAlreadyExists test for authService

--- src/test/java/com/macias34/codemastery/service/AuthServiceTest.java
+++ src/test/java/com/macias34/codemastery/service/AuthServiceTest.java
@@ -1,24 +1,25 @@
 package com.macias34.codemastery.service;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.macias34.codemastery.auth.dto.SignUpDto;
 import com.macias34.codemastery.auth.service.AuthService;
-import com.macias34.codemastery.user.entity.UserEntity;
+import com.macias34.codemastery.exception.ResourceAlreadyExistsException;
 import com.macias34.codemastery.user.repository.UserRepository;
 
 public class AuthServiceTest {
 	@Test
-	public void shouldCreateUserAndCallSaveOnRepository() {
-
+	public void shouldThrowExceptionWhenUserAlreadyExists() {
 		UserRepository userRepository = mock(UserRepository.class);
 		PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
+
+		when(userRepository.existsByUsername(anyString())).thenReturn(true);
 
 		SignUpDto signUpDto = new SignUpDto();
 		signUpDto.setUsername("username");
@@ -26,8 +27,10 @@ public class AuthServiceTest {
 		signUpDto.setPassword("password");
 
 		AuthService authService = new AuthService(userRepository, passwordEncoder, null, null);
-		authService.createUser(signUpDto);
 
-		verify(userRepository, times(1)).save(any(UserEntity.class));
+		assertThrows(ResourceAlreadyExistsException.class, () -> {
+			authService.createUser(signUpDto);
+		});
 	}
+
 }
