91c7feeedc431b125f244203067fd10d5e78bd3f
macias34 <propiesek@gmail.com>
2023-09-27 19:44:55 +0200

    feat: added Swagger

--- src/main/java/com/macias34/codemastery/CodeMasteryApplication.java
+++ src/main/java/com/macias34/codemastery/CodeMasteryApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvBuilder;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+// @OpenAPIDefinition(servers = {
+// 		@Server(url = "/api/v1/", description = "Default Server URL")
+// })
 @SpringBootApplication
 public class CodeMasteryApplication {
 
