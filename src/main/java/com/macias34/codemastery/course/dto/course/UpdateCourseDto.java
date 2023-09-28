372562f8ba83017ab4b6cdb3ec01e9082c8e314e
macias34 <propiesek@gmail.com>
2023-09-28 11:39:17 +0200

    feat: added validation rules to UpdateCourseDto

--- src/main/java/com/macias34/codemastery/course/dto/course/UpdateCourseDto.java
+++ src/main/java/com/macias34/codemastery/course/dto/course/UpdateCourseDto.java
@@ -2,6 +2,7 @@ package com.macias34.codemastery.course.dto.course;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,13 +14,16 @@ import java.util.Set;
 @AllArgsConstructor
 public class UpdateCourseDto {
     @Nullable
+    @Size(min = 3, max = 50)
     private String name;
     @Min(0)
     @Nullable
     private Double price;
     @Nullable
+    @Size(min = 3, max = 50)
     private String instructorName;
     @Nullable
+    @Size(min = 3, max = 2000)
     private String description;
     @Nullable
     private Set<Integer> categoriesIds;
